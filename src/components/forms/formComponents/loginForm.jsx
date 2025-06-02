bba6925c49166731c47dc7a291d452ebcbb70e03
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-06-02 17:08:35 +0530

    validation on form input using regexp and useState

--- src/components/forms/formComponents/loginForm.jsx
+++ src/components/forms/formComponents/loginForm.jsx
@@ -1,33 +1,73 @@
-import { useState } from "react";
+import {  useState } from "react";
 
 
 const LoginFrom =() =>{
     
+    const [ email, setEmail] = useState("");
+    const [ password, setPassword] = useState("")
+    const [ emailError, setEmailError] = useState("");
+    const [ passwordError, setPasswordError] = useState("");
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex= /.{8}/;
+    const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+
+    if (!emailRegex.test(value)) {
+      setEmailError("Invalid email format");
+    } else {
+      setEmailError("");
+    }
+  };
+
+
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+
+    if(!passwordRegex.test(value)){
+        setPasswordError("password must be atleast 8 digits long ")
+    } else {
+        setPasswordError("");
+    }
+  };
+
+
     return(
         <div className="py-4 py-xl-4 mt-lx-3">
             <div className="row gy-lg-3 gy-xl-4">
                 <div className="col-12  col-md-6 col-lg-6 col-xl-5 mx-auto px-md-4 px-4">
-                    <form>
-                       <>
+                    <form action=" "> 
+                    
                        <p className="text-center fs-4">Login</p>
                         <div className="form-floating mb-3">
                             <input
                             type="email"
                             className="form-control"
-                            placeholder=""
+                            placeholder="kotak@gmail.com"
+                            required
+                            value={email}
+                            // pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+                            onChange={handleEmailChange}
                             />
                             <label htmlFor="">Email address</label>
+                            {emailError && <p className="text-danger">{emailError}</p>}
                         </div>
                         <div className="form-floating">
                             <input
                             type="password"
                             className="form-control"
                             placeholder=""
+                            value={password}
+                            onChange={handlePasswordChange}
+                            required
                             />
                             <label >Password</label>
+                            { passwordError && <p className="text-danger">{passwordError}</p>}
                         </div>
-                        <div className="btn form-control mt-4 blueish-btn text-white shadow-soft" type="submit">Login</div>
-                        </>
+                        <button className="btn form-control mt-4 blueish-btn text-white shadow-soft" type="submit">Login</button>
+                        
                     </form>
                 </div>
             </div>
@@ -36,6 +76,52 @@ const LoginFrom =() =>{
 }
 
 const RegisterNew = () =>{
+
+
+    const [ email, setEmail] = useState("");
+    const [ phone, setPhone] = useState("");
+    const [ password, setPassword ] = useState("")
+    const [ emailError , setEmailerror] = useState("")
+    const [phoneError ,setPhoneError] = useState("")
+    const [ passwordError, setPasswordError] = useState("")
+    
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /.{8}/;
+    const phoneRegex = /[0-9]{10}/;
+
+
+    const handleEmailChange = (e) => {
+        const value = e.target.value;
+        setEmail(value)
+        if(!emailRegex.test(value)){
+            setEmailerror("invalid format")
+        } else {
+            setEmailerror("")
+        }
+    }
+    const handlePhoneChange =(e) =>{
+        const value = e.target.value;
+        setPhone(value)
+
+        if(!phoneRegex.test(phone)){
+            setPhoneError("invalid number")
+        } else {
+            setPhoneError("");
+        }
+    }
+
+    const handlePasswordChange = (e) =>{
+        const value = e.target.value;
+        setPassword(value);
+
+        if(!passwordRegex.test(value)){
+            setPasswordError("invalid format")
+        } else {
+            setPasswordError("");
+        }
+    }
+
     return(
         <div className="py-4 my-xl-5">
             <div className="row gy-3">
@@ -47,25 +133,34 @@ const RegisterNew = () =>{
                             <input
                                 type="email"
                                 className="form-control"
-                                placeholder=""
+                                placeholder="Enter you email"
+                                onChange={handleEmailChange}
+                                value={email}
                             />
                             <label htmlFor="">Email address</label>
+                            {emailError && <p className="text-danger">{emailError}</p>}
                         </div>
                         <div className="form-floating mb-3">
                             <input
                                 type="number"
                                 className="form-control"
                                 placeholder=""
+                                value={phone}
+                                onChange={handlePhoneChange}
                             />
                             <label >Enter your Phone number</label>
+                            {phoneError && <p className="text-danger">{phoneError}</p>}
                         </div>
                         <div className="form-floating">
                             <input
                                 type="password"
                                 className="form-control"
                                 placeholder=""
+                                value={password}
+                                onChange={handlePasswordChange}
                             />
                             <label >Password</label>
+                            {passwordError && <p className="text-danger">{passwordError}</p>}
                         </div>
                         <div className="btn form-control mt-4 blueish-btn text-white shadow-soft" type="submit">Login</div>
                         </>
